9abddba0e62f3ec1baa1123ec64900be523146c3
zhaoxiangbiao <443621880@qq.com>
2017-02-28 14:11:57 +0800

    提交spring boot项目

---
+++ src/main/java/cn/zxb/SpringboottestApplication.java
@@ -0,0 +1,20 @@
+package cn.zxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringboottestApplication {
+	
+	@RequestMapping("/")
+    String home(){
+        return "hello spring boot";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestApplication.class, args);
+	}
+}
